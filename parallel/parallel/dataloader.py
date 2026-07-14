60ccd77a5a8e587b66674f1b578e1071fdadf273
Jonah Samost <jonah.samost@horizon3.ai>
2026-07-14 10:54:25 -0700

    dataloader

--- parallel/parallel/dataloader.py
+++ parallel/parallel/dataloader.py
@@ -1,3 +1,4 @@
+from parallel.parallel.dataset import list_data_files
 import torch
 import pyarrow.parquet as pq
 
@@ -9,3 +10,101 @@ def _create_batches(
     tokenizer_batch_size: int,
 ):
     state = RuntimeState()
+    file_paths = list_data_files()
+    assert len(file_paths) != 0, "No data found. please run dataset.py"
+    file_paths = file_paths[:-1] if split == "train" else file_paths[-1:]
+
+    resume_pq_idx = resume_state_dict["pq_idx"] if resume_state_dict is not None else 0
+    resume_rg_idx = resume_state_dict["rg_idx"] if resume_state_dict is not None else None
+    resume_epoch = resume_state_dict.get("epoch", 1) if resume_state_dict is not None else 1
+    first_pass = True
+    pq_idx = resume_pq_idx
+    epoch = resume_epoch
+
+    while True:
+        pq_idx = resume_pq_idx if first_pass else 0
+        while pq_idx < len(file_paths):
+            filepath = file_paths[pq_idx]
+            pf = pq.ParquetFile(filepath)
+            if first_pass and (resume_rg_idx is not None) and (pq_idx == resume_pq_idx):
+                base_idx = resume_rg_idx // state.world_size
+                base_idx += 1
+                rg_idx = base_idx * state.world_size + state.rank # TODO is this right for > 1 node? 
+                if rg_idx >= pf.num_row_groups:
+                    pq_idx += 1
+                    continue
+                resume_rg_idx = None
+            else:
+                rg_idx = state.rank
+            while rg_idx < pf.num_row_groups:
+                rg = pf.read_row_group(rg_idx)
+                batch = rg.column("text").to_pylist()
+                for i in range(0, len(batch), tokenizer_batch_size):
+                    yield batch[i: i + tokenizer_batch_size], (pq_idx, rg_idx, epoch)
+                rg_idx += state.rank
+            pq_idx += 1
+        first_pass = False
+        epoch += 1
+
+
+def dist_data_loader(
+    tokenizer, B, T, split,
+    tokenizer_threads = 4, tokenizer_batch_size=128,
+    device="cuda", resume_state_dict=None, buffer_size=1000,
+):
+    assert split in ["train", "val"], "split must be train/val"
+
+    row_capacity = T + 1
+    batches = _create_batches(split, resume_state_dict, tokenizer_batch_size)
+    bos_token = tokenizer.get_bos_token_id()
+    doc_buffer = []
+    pq_idx, rg_idx, epoch = 0, 0, 1
+
+    def refill_buffer():
+        nonlocal pq_idx, rg_idx, epoch
+        doc_batch, (pq_idx, rg_idx, epoch) = next(batches)
+        token_lists = tokenizer.encode(doc_batch, prepend=bos_token, num_threads=tokenizer_threads)
+        for tokens in token_lists:
+            doc_buffer.append(tokens)
+    
+    use_cuda = device == "cuda"
+    row_buffer = torch.empty((B, row_capacity), dtype=torch.long) # for building rows without creating Python lists
+    cpu_buffer = torch.empty(2 * B * T, dtype=torch.long, pin_memory=use_cuda) # staging area (CPU)
+    gpu_buffer = torch.empty(2 * B * T, dtype=torch.long, device=device) # on-device buffer
+    cpu_inputs = cpu_buffer[:B * T].view(B, T) # a few views into these buffers just for convenience
+    cpu_targets = cpu_buffer[B * T:].view(B, T)
+    inputs = gpu_buffer[:B * T].view(B, T)
+    targets = gpu_buffer[B * T:].view(B, T)
+
+    while True:
+        for row_idx in range(B):
+            pos = 0
+            while pos < row_capacity:
+                while len(doc_buffer) < buffer_size:
+                    refill_buffer()
+                remaining = row_capacity - pos
+
+                best_idx = -1
+                best_len = 0
+                for i, doc in enumerate(doc_buffer):
+                    doc_len = len(doc)
+                    if doc_len <= remaining and doc_len > best_len:
+                        best_idx = i
+                        best_len = doc_len
+                
+                if best_idx >= 0:
+                    doc = doc_buffer.pop(best_idx)
+                    doc_len = len(doc)
+                    row_buffer[row_idx, pos: pos + doc_len] = torch.tensor(doc, dtype=torch.long)
+                    pos += doc_len
+                else:
+                    shortest_idx = min(range(len(doc_buffer)), key=lambda i: len(doc_buffer[i]))
+                    doc = doc_buffer.pop(shortest_idx)
+                    row_buffer[row_idx: pos:pos + remaining] = torch.tensor(doc[:remaining], dtype=torch.long)
+                    pos += remaining
+
+        cpu_inputs.copy_(row_buffer[:, :-1])
+        cpu_targets.copy_(row_buffer[:, 1:])
+        state_dict = {"pq_idx": pq_idx, "rg_idx": rg_idx, "epoch": epoch}
+        gpu_buffer.copy_(cpu_buffer, non_blocking=use_cuda)
+        yield inputs, targets, state_dict
